2451ac0f3834245675bb9ddd440335d90c055e6f
Adnan Sadiq <adnansadiqxyz@gmail.com>
2022-07-29 16:30:10 +0500

    complted

--- src/components/News.js
+++ src/components/News.js
@@ -5,7 +5,7 @@ import '../css/style1.css'
 import newsImg from '../images/newsimg.jpg'
 import Spiner from './Siner';
 import Error from './Error'
-// import InfiniteScroll from 'react-infinite-scroll-component';
+import InfiniteScroll from 'react-infinite-scroll-component';
 const News =(props)=> {
    const [articles, setArticles]=useState([])
    const [loading, setLoading]=useState(true)
@@ -13,25 +13,14 @@ const News =(props)=> {
    const [errorFlag, setErrorFlag]=useState(0)
    const [totalPages, setTotalPages]=useState(1)
    const [pageNo, setPageNo]=useState(1)
+   const [totalResults, setTotalResults]=useState(0)
 
 
 
 
 
    
-    
 
-   const CheckError =(response)=> {
-        if (response.status >= 200 && response.status <= 299) {
-            // setErrorFlag(1)
-          return response;
-        } else {
-            
-            setErrorFlag(1)
-          
-          
-        }
-      }
     const updateData= async (pageNo)=>
     {
         props.setProgress(10)
@@ -40,27 +29,20 @@ const News =(props)=> {
 
        
         setLoading(true)
-        let resp=await fetch(url).then(CheckError)
-        if(resp===0)
-        {
-            console.log("error")
-    
-        }
-        else{
+        let resp=await fetch(url)
+        props.setProgress(50)
             let respData=await resp.json();
          
             setArticles(respData.results)
             setLoading(false)
             setTotalPages(respData.totalResults/props.pageSize)
-            
-           
-        }
-        props.setProgress(100)
-
+            setTotalResults(respData.totalResults)
+            props.setProgress(100)
+      
 
     }
     useEffect(()=>{
-        console.log("hlo")
+
         updateData(1)
     },[])
  
@@ -76,43 +58,8 @@ const News =(props)=> {
         setPageNo(pageNo-1)
         updateData(pageNo)
     }
-    // fetchMoreData = async()=>{
-        
-    //     this.setState({
-    //         page:this.state.page+1
-    //     })
-    //     console.log(this.state.page)
-    //     let url=`https://newsdata.io/api/1/news?apikey=pub_9628988648c41baa85577f0d420f3461ca56&page=${this.state.page}&category=${this.props.category}`;
 
-    //     this.setState({
-    //         loading:true
-    //     })
-    //     let resp=await fetch(url).then(this.CheckError)
-    //     if(resp===0)
-    //     {
-    //         console.log("error")
-    
-    //     }
-    //     else{
-    //         let respData=await resp.json();
-    //         this.setState({
-    //             articles:this.state.articles.concat( respData.results),
-    //             totalPages:respData.totalResults/this.props.pageSize,
-    //             loading:false,
-    //             totalResults:respData.totalResults
-    //         })
-            
-    //         console.log(this.state.articles)
-    //     }
-    // }
-
-  
-  
-    if(errorFlag===0)
-    {
-
-    
-        if(loading==false)
+  if(loading==false)
         {   
         
             return (
@@ -120,30 +67,17 @@ const News =(props)=> {
       
                 <div>
         
-                <h1 style={{textAlign:'center'}}>{`Top Headlines on ${props.category}`}</h1>
+                <h1 style={{textAlign:'center' , marginTop:'5rem'}}>{`Top Headlines on ${props.category}`}</h1>
 
-                 {/* <InfiniteScroll
-                  dataLength={this.state.articles.length}
-                  
-                   
-                   // console.log(this.state.articles.length)
-                  
-                  next={this.fetchMoreData}
-                  style={{ display: 'flex', flexDirection: 'column-reverse' }} //To put endMessage and loader to the top.
-                  inverse={true} //
-                  hasMore={this.state.articles.length !== this.state.totalResults}
-                  loader={<h4>Loading...</h4>}
-                  scrollableTarget="scrollableDiv"
-                > */}
+             
 
                    <div className="news-container">
                     {
-                    articles.map((element)=>{
-                    return <NewsItem key={element.url} title={element.title} description={element.description} imgurl={element.image_url !=null?element.image_url:newsImg} uniqeUrl={element.link} pubDate={element.pubDate} source_id={element.source_id} />
+                    articles.map((element,index)=>{
+                    return <NewsItem key={index} title={element.title} description={element.description} imgurl={element.image_url !=null?element.image_url:newsImg} uniqeUrl={element.link} pubDate={element.pubDate} source_id={element.source_id} />
                     })
                     }
                    </div>
-                {/* </InfiniteScroll>  */}
                
                 <div className="btn-cont">
                 <button disabled={pageNo<=1} type='button' className='btn btn-primary' onClick={handlePreviousClick} >&larr; Previous</button>
@@ -157,12 +91,7 @@ const News =(props)=> {
            <Spiner/>
          )
         }
-    }
-    else{
-        return (
-            <Error/>
-        )
-    }
+
   
 }
 
